9c821740f1d8b410115bfc947dada2cc9854b03d
Nguyễn Văn Tiến <54173812+Nightmare457@users.noreply.github.com>
2021-07-18 12:37:22 +0700

    Folder tree + Server + Enum

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/CS426FinalProject
 
 go 1.16
+
+require (
+	github.com/joho/godotenv v1.3.0
+	github.com/labstack/echo/v4 v4.4.0
+	go.mongodb.org/mongo-driver v1.6.0
+)
--- main.go
+++ main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/CS426FinalProject/api"
+	"github.com/CS426FinalProject/controller"
+	"github.com/CS426FinalProject/db"
+	"github.com/CS426FinalProject/enum"
+	"github.com/CS426FinalProject/model"
+	"github.com/CS426FinalProject/repo"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
+func hello() {
+	fmt.Println("Hello World!")
+}
+
 func main() {
-	fmt.Print("Hello world!")
+	server := api.InitServer()
+	godotenv.Load(".env")
+	DB_URI := os.Getenv("DB_URI")
+	server.SetGroup("/", hello)
 }
