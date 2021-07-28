9d66a1dc83280166070bb7f61e812a093a7047bf
CuzzImBatman <lgbao19@apcs.vn>
2021-07-28 15:05:28 +0700

    Update test_controller.go

--- controller/test_controller.go
+++ controller/test_controller.go
@@ -3,8 +3,10 @@ package controller
 import (
 	"fmt"
 
-	"github.com/CS426FinalProject/api"
-	"github.com/CS426FinalProject/enum"
+	"github.com/Cumbercubie/api"
+	"github.com/Cumbercubie/enum"
+	"github.com/Cumbercubie/model"
+	"github.com/Cumbercubie/repo"
 	"github.com/labstack/echo/v4"
 )
 
@@ -19,14 +21,30 @@ func TestControllerGroup(g *echo.Group) error {
 // Testing root path of user page
 func TestPage(c echo.Context) error {
 	api.Respond(c, &enum.APIResponse{
-		Status: enum.APIStatus.Ok,
+		Status:  enum.APIStatus.Ok,
 		Message: fmt.Sprintf("Test Page"),
 	})
 	return nil
 }
 
 // Create a test
-func CreateTestAction(c echo.Context) error {
 
+func CreateTestAction(c echo.Context) error {
+	var body model.Test
+	err := api.GetContent(c, &body)
+	if err != nil {
+		return api.Respond(c, &enum.APIResponse{
+			Status:  enum.APIStatus.Invalid,
+			Message: "Can not parse input data",
+		})
+	}
+	insertErr := repo.CreateTest(body)
+	if insertErr != nil {
+		return api.Respond(c, &enum.APIResponse{
+			Status:  enum.APIStatus.Error,
+			Message: fmt.Sprintf("Error inserting test: %s", insertErr.Error()),
+		})
+	}
 	return nil
+
 }
