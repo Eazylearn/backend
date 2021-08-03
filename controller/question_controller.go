0688166f79925b988173425b04fd09d94a2bbd05
CuzzImBatman <lgbao19@apcs.vn>
2021-08-03 19:23:34 +0700

    change

--- controller/question_controller.go
+++ controller/question_controller.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/CS426FinalProject/api"
 	"github.com/CS426FinalProject/enum"
+	"github.com/CS426FinalProject/model"
+	"github.com/CS426FinalProject/repo"
 	"github.com/labstack/echo/v4"
 )
 
@@ -26,7 +28,23 @@ func QuestionPage(c echo.Context) error {
 }
 
 // Create a test
-func CreateQuestionAction(c echo.Context) error {
 
+func CreateQuestionAction(c echo.Context) error {
+	var body model.Question
+	err := api.GetContent(c, &body)
+	if err != nil {
+		return api.Respond(c, &enum.APIResponse{
+			Status:  enum.APIStatus.Invalid,
+			Message: "Can not parse input data",
+		})
+	}
+	insertErr := repo.CreateQuestion(body)
+	if insertErr != nil {
+		return api.Respond(c, &enum.APIResponse{
+			Status:  enum.APIStatus.Error,
+			Message: fmt.Sprintf("Error inserting Quétion: %s", insertErr.Error()),
+		})
+	}
 	return nil
+
 }
