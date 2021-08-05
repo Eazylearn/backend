b037cdc431dbfd6c05631c00847cba8693f2ee42
CuzzImBatman <lgbao19@apcs.vn>
2021-08-05 21:29:22 +0700

    Revert "Merge pull request #3 from CS426FinalProject/feature/test-API"
    This reverts commit 46f271b8ca1ce5862d6251b116ce6c11028f3810, reversing
    changes made to 1a31c11f4de3bd64a6373bf11a150ab7e8ca7a08.

--- repo/test_repo.go
+++ repo/test_repo.go
@@ -2,11 +2,8 @@ package repo
 
 import (
 	"context"
-	"fmt"
-	"log"
 
 	"github.com/CS426FinalProject/model"
-	"go.mongodb.org/mongo-driver/bson"
 )
 
 func CreateTest(test model.Test) error {
@@ -16,20 +13,4 @@ func CreateTest(test model.Test) error {
 		return err
 	}
 	return nil
-}
-
-func GetTestByID(id int64) (model.Test, error) {
-	var test model.Test
-	result, qErr := model.UserDB.Collection.Find(context.TODO(), bson.M{})
-	if qErr != nil {
-		log.Println("test_repo.go/GetTestByID: Error finding", qErr.Error())
-		return test, qErr
-	}
-	fmt.Println(result == nil)
-	err := result.All(context.TODO(), &test)
-	if err != nil {
-		log.Println("test_repo.go/GetTestByID: Error encoding", err.Error())
-		return test, err
-	}
-	return test, nil
 }
